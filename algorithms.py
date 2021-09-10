a13cfd57ecc0845b9b53dc1c8fad693d3ba77669
Alex4849 <karneenko988@gmail.com>
2021-09-10 10:37:48 +0300

    merge lists

--- algorithms.py
+++ algorithms.py
@@ -58,7 +58,7 @@ fac(1)
 
 
 # Бинарный поиск
-
+"""
 def binary_search(x, y):                
     low = 0                               
     high = len(x)
@@ -78,3 +78,26 @@ def binary_search(x, y):
 
 x = [0,1,2,3,4,5,6,7,8,9,10]
 print(binary_search(x, 8))
+"""
+
+# Слияние списков
+
+
+
+n, m = 3,5
+a = [2,8,8] 
+b = [3,4,5,5,10]
+i = j =0
+c =[]
+while i<n and j<m:
+    if a[i]< b[j]:
+        c.append(a[i])
+        i +=1
+    else:
+        c.append(b[j])
+        j+=1
+while j<m:
+    c.append(b[j])
+    j+=1
+
+    print(*c)
