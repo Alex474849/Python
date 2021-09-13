d3104821f2e655b3e5a0c6635d7f2579d8dee65f
Alex4849 <karneenko988@gmail.com>
2021-09-13 11:36:49 +0300

    quick sort

--- algorithms.py
+++ algorithms.py
@@ -104,7 +104,7 @@ while j<m:
 """
 
 # Слияние списков через рекурссию
-
+"""
 def merge_lists(a, b):
     c= []
     i=j=0
@@ -131,3 +131,19 @@ def merge(s):           # функция merge выполняет сортиро
 
 
 print(merge([7,5,2,3,9,8]))
+"""
+
+# Быстрая сортировка
+
+
+def quick_sort(s):
+    if len(s)<= 1:
+        return s
+    elem = s[0]
+    left = list(filter(lambda x: x<elem, s))
+    center = [i for i in s if i== elem ]
+    right = list(filter(lambda x: x>elem, s)) 
+
+    return quick_sort(left) + center + quick_sort(right)  
+
+print(quick_sort([7,6,10,5,9,8,3,4]))
